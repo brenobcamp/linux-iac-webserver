6e0b0233154b8b9748c825d4cef124b4dfc709db
Breno Batista Campos <108765626+brenobcamp@users.noreply.github.com>
2022-08-10 19:26:59 -0300

    Update web-server.sh

--- web-server.sh
+++ web-server.sh
@@ -22,7 +22,7 @@ wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.z
 echo "Copiando arquivos para o Apache..."
 
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 # Troque "linux-site-dio" para o nome do seu repositório
 cp * /var/www/html/ 
 echo "Finalizado!"
